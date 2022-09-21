449352d00aa0831fe27b091436a0cb44fc803f54
Renan Amorim <renan.net2014@gmail.com>
2022-09-21 00:40:36 -0300

    salvando antes do merge

---
+++ lib/view/login.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:prototipo_navegacao/util/routes.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  //controladores das caixas de texto
+  final _ctrUsuario = TextEditingController();
+  final _ctrSenha = TextEditingController();
+  bool exibirSenha = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Container(
+            constraints: const BoxConstraints(
+              maxWidth: 500,
+              maxHeight: double.infinity,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //TextBox de usuário
+                TextFormField(
+                  controller: _ctrUsuario,
+                  decoration: const InputDecoration(
+                    labelText: "Usuário",
+                    prefixIcon: Icon(Icons.person_rounded),
+                  ),
+                ),
+                //TextBox de senha
+                TextFormField(
+                  controller: _ctrSenha,
+                  obscureText: !exibirSenha,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: InputBorder.none,
+                    prefixIcon: const Icon(Icons.lock_rounded),
+                    suffixIcon: InkWell(
+                      onTap: () => setState(
+                        () => exibirSenha = !exibirSenha,
+                      ),
+                      child: Icon(
+                        exibirSenha
+                          ? Icons.visibility_outlined
+                          : Icons.visibility_off_outlined,
+                        color: const Color(0xFF757575),
+                      ),
+                    ),
+                  ),
+                ),
+                //Botão de login
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_ctrUsuario.text == "exemplo@email.com" &&
+                          _ctrSenha.text == "123") {
+                        Navigator.of(context).pushReplacementNamed(Routes.homePage);
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context){
+                            return const AlertDialog(
+                              title: Text('Algo deu errado'),
+                              content: Text('Verifique os dados de login'),
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: const Text("Entrar"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
---
+++ lib/view/menu_principal.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/default_user_drawer.dart';
+
+class MenuPrincipalView extends StatefulWidget {
+  const MenuPrincipalView({Key? key}) : super(key: key);
+
+  @override
+  State<MenuPrincipalView> createState() => _MenuPrincipalViewState();
+}
+
+class _MenuPrincipalViewState extends State<MenuPrincipalView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const DefaultUserDrawer(),
+      appBar: AppBar(),
+      body: SizedBox(
+        child: Column(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [ //atenção ao const
+                //Texto de boas vindas
+                Padding(
+                  padding: EdgeInsets.only(top: 25, left: 50),
+                  child: Text(
+                    "Olá, bem vindo\nao sistema",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
